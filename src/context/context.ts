96db1f6a8bb31f913024b624daff796fa694d59f
Monalisa Baltatescu <monalisa.baltatescu@accesa.eu>
2022-04-03 21:35:20 +0300

    Removed unused context

--- src/context/context.ts
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import { IOverallResult } from "../models/IOverallResult";
-
-const QuizContext = React.createContext({
-    overallResult: IOverallResult.Incorrect
-});
-
-export default QuizContext;
